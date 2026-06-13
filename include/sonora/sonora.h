b9582422c72cb2d4a3e6fe769035020939493486
Jorge Andrés Jiménez <imnighty201@gmail.com>
2026-06-13 13:22:22 -0500

    minimal test

---
+++ include/sonora/sonora.h
@@ -0,0 +1,85 @@
+#ifndef SONORA_H
+#define SONORA_H
+
+#include <stdint.h> //for uint64_t
+#include "s_deviceinfo.h"
+
+#if defined(__cplusplus)
+	#include <vector>
+	#include <string>
+	#include <optional>
+	#include <variant>
+#endif
+
+//TODO: C COMPATIBILITY
+
+typedef struct Format {
+	uint16_t	formatTag;
+	uint16_t	channels;
+	uint32_t	sampleRate;
+	/// Specifies the required average data transfer rate in bytes per second. 
+	/// This value is useful for estimating buffer size. BLOCK_ALIGN * SAMPLE_RATE;
+	uint32_t	avgBytesPerSec;
+	/// If wFormatTag = WAVE_FORMAT_PCM or wFormatTag = WAVE_FORMAT_IEEE_FLOAT, 
+	/// set nBlockAlign to (Channels*BitsPerSample)/8, which is the size of a 
+	/// single audio frame. 
+	uint16_t    blockAlign;
+	/// If wFormatTag = WAVE_FORMAT_PCM, then wBitsPerSample 
+	/// should be set to either 8 or 16. 
+	/// If wFormatTag = WAVE_FORMAT_IEEE_FLOAT, 
+	/// wBitsPerSample should be set to 32.
+	uint16_t	bitsPerSample;
+	uint16_t    cbSize; //extra information (after cbSize)
+} Format;
+
+// Structure containing device handle
+#if defined(_WIN32)
+	typedef void* Handle; //HWAVEOUT in Windows
+#endif
+
+#define SONORA_DEFAULT_DEVICE			0
+
+typedef struct OutDevice {
+	Handle handle;
+	bool isOpen;
+	uint32_t id;
+} OutDevice;
+
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+	const wchar_t **getOutDeviceNameList(uint64_t* outNumDevs);
+	const wchar_t  *getOutDeviceNameByIdx(uint64_t i);
+#if defined(__cplusplus)
+} // extern "C"
+
+//TODO: open and close out and in devices with AnyDevice
+//TODO: get device list and devices with AnyDevice 
+//TOOD: inDevice (I HAVE NO IDEA HOW DOES THAT WORK MAN)
+//TODO: fromFile(FILE) FILE C type
+
+namespace sonora {
+	using DevicePtr = std::variant<OutDevice*>;
+
+	auto       getOutDeviceNameList()               -> std::vector<std::wstring>;
+	auto       getOutDeviceNameById(uint32_t index) -> std::optional<std::wstring>;
+
+	bool	   openOutDevice(Format *engine, OutDevice *outDevice);
+	bool	   closeDevice(DevicePtr device);
+	bool       closeFormat(Format* engine);
+	bool	   playSound(Format* engine, OutDevice* device, const char* path);
+
+	Format*    newFormat(); //44.1Hz, Stereo, 16bits
+	Format*    newFormat(uint32_t format);
+	Format*    newFormat(uint16_t formatTag, uint16_t channels, uint32_t sampleRate, uint16_t bitsPerSample);
+
+	OutDevice* newOutDevice(); //WAVE_MAPPER
+	OutDevice* newOutDevice(uint32_t index);
+
+}	
+#endif // defined(__cplusplus)
+
+#endif // !SONORA_H
+
+
--- test/sonora-tests.cpp
+++ test/sonora-tests.cpp
@@ -1,12 +1,6 @@
 #include "../include/sonora/sonora.h"
 #include "external/catch_amalgamated.hpp"
 
-#include <iostream>
-#include <memory>
-#include <fstream>
-#include <string>
-#include <chrono>
-
-TEST_CASE("TRUE") {
+TEST_CASE("TRUE", "ede") {
 
 }
